1bc4cdb0fde0488bb99315600f70f23c4ad9d349
qumn <wang247657@163.com>
2024-05-29 18:00:00 +0800

    luasnip unlick when mode change

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are aunntomatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- TODO waiting merge remove the autocmd
+-- Use the more sane snippet session leave logic. Copied from:
+-- https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1429989436
+-- https://github.com/LazyVim/LazyVim/pull/2519/commits/5a708b199e9d487dd407ec3207e71b863371ad0d
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function()
+    if
+      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
+})
