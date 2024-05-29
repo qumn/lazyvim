26fcff5c632d747ce14324cc313eec5161424e53
qumn <2476573497@qq.com>
2024-05-29 23:36:23 +0800

    add tmux support

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -15,20 +15,11 @@ return {
   },
   init = function()
     local map = vim.keymap.set
-    if vim.g.neovide then
       -- stylua: ignore start
       map("n", "<c-y>", function() require("tmux").move_left() end)
       map("n", "<c-n>", function() require("tmux").move_bottom() end)
       map("n", "<C-i>", function() require("tmux").move_top() end)
       map("n", "<c-o>", function() require("tmux").move_right() end)
-      -- stylua: ignore end
-    else
-      vim.cmd([[
-      nnoremap <C-y> <C-w>h
-      nnoremap <C-n> <C-w>j
-      nnoremap <C-i> <C-w>k
-      nnoremap <C-o> <C-w>l
-      ]])
-    end
+    -- stylua: ignore end
   end,
 }
