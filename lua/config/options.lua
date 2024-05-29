1618bbb96282b3010007d657e52ca42b69000694
qumn <wang247657@163.com>
2024-05-29 18:00:20 +0800

    set neovide font

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,34 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+--
+vim.g.gui_font_default_size = 18
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = "CaskaydiaCove Nerd Font"
+
+-- Keymaps
+vim.keymap.set("n", "<C-=>", function()
+  ResizeGuiFont(1)
+end)
+vim.keymap.set("n", "<C-->", function()
+  ResizeGuiFont(-1)
+end)
+
+-- gui specialize config
+RefreshGuiFont = function()
+  vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+  vim.g.gui_font_size = vim.g.gui_font_size + delta
+  RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
