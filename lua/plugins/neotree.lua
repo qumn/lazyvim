22fd1b95e6c0ef13e855b7c0a643508a5ef9b0fd
qumn <2476573497@qq.com>
2024-05-29 23:36:55 +0800

    refactor neo-tree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,23 +1,21 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  config = function()
-    require("neo-tree").setup({
-      window = {
-        mappings = {
-          ["i"] = "noop",
-          ["o"] = {
-            "open",
-            nowait = true,
-          },
-          ["oc"] = "noop",
-          ["od"] = "noop",
-          ["og"] = "noop",
-          ["om"] = "noop",
-          ["on"] = "noop",
-          ["os"] = "noop",
-          ["ot"] = "noop",
+  opts = {
+    window = {
+      mappings = {
+        ["i"] = "noop",
+        ["o"] = {
+          "open",
+          nowait = true,
         },
+        ["oc"] = "noop",
+        ["od"] = "noop",
+        ["og"] = "noop",
+        ["om"] = "noop",
+        ["on"] = "noop",
+        ["os"] = "noop",
+        ["ot"] = "noop",
       },
-    })
-  end,
+    },
+  },
 }
