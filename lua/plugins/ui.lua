ca897918dd7963c529a3c753d12377c2f68baa01
qumn <wang247657@163.com>
2024-05-30 17:58:27 +0800

    add ui file

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "storm" },
+  },
+  -- {
+  --   "sainnhe/everforest",
+  --   -- version = false,
+  --   -- lazy = false,
+  --   -- priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- Optional; default configuration will be used if setup isn't called.
+  --   config = function() end,
+  -- },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   opts = {
+  --     flavour = "macchiato",
+  --   },
+  -- },
+  --
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   opts = {
+  --     theme = "catppuccin",
+  --   },
+  -- },
+  --
+  -- -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
+}
