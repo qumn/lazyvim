3abf31a679ae6e8b620630d429e43c3ca7d48fa6
qumn <wang247657@163.com>
2024-07-30 14:35:37 +0800

    disable mini.ai keymap

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.ai",
+    opts = {
+      mappings = {
+        around_next = "",
+        inside_next = "",
+        around_last = "",
+        inside_last = "",
+      },
+    },
+    enable = false,
+  },
+}
