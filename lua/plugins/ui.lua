42aef2b72767a4a3f7a998c8a3c025b1ca765624
qumn <2476573497@qq.com>
2024-12-31 15:45:11 +0800

    add window picker plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -40,4 +40,13 @@ return {
       colorscheme = "everforest",
     },
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
 }
