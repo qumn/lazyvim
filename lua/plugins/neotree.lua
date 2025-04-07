339ee1b1df74abbd96a5c018f3753ececed7c461
qumn <wang247657@163.com>
2025-04-07 13:45:33 +0800

    update

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,25 +1,33 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      group_empty_dirs = true,
-      scan_mode = "deep",
-    },
-    window = {
-      mappings = {
-        ["i"] = "noop",
-        ["o"] = {
-          "open",
-          nowait = true,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        group_empty_dirs = true,
+        scan_mode = "deep",
+      },
+      window = {
+        mappings = {
+          ["i"] = "noop",
+          ["o"] = {
+            "open",
+            nowait = true,
+          },
+          ["oc"] = "noop",
+          ["od"] = "noop",
+          ["og"] = "noop",
+          ["om"] = "noop",
+          ["on"] = "noop",
+          ["os"] = "noop",
+          ["ot"] = "noop",
         },
-        ["oc"] = "noop",
-        ["od"] = "noop",
-        ["og"] = "noop",
-        ["om"] = "noop",
-        ["on"] = "noop",
-        ["os"] = "noop",
-        ["ot"] = "noop",
       },
     },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    config = function()
+      vim.g.mkdp_echo_preview_url = true
+    end,
   },
 }
