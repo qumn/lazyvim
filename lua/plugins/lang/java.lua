59b2cb4a804999533cfb6a7387d2554e1f6e47ee
qumn <wang247657@163.com>
2025-06-02 16:23:18 +0800

    update

--- lua/plugins/lang/java.lua
+++ lua/plugins/lang/java.lua
@@ -5,8 +5,8 @@ return {
     opts = {
       jdtls = function(opts)
         local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
-        local jvmArg = "-javaagent:" .. install_path .. "/lombok.jar"
-        table.insert(opts.cmd, "--jvm-arg=" .. jvmArg)
+        local args = "-javaagent:" .. install_path .. "/lombok.jar"
+        table.insert(opts.cmd, "--jvm-arg=" .. args)
         return opts
       end,
     },
