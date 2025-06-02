8714fd6f8d8baab2aa9379c553781d24c71d5c60
qumn <wang247657@163.com>
2025-06-02 16:10:02 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,9 +5,12 @@
 --
 vim.opt.list = true
 vim.opt.listchars = "tab:> ,trail:▫"
+vim.opt.termguicolors = true
 
 vim.g.gui_font_default_size = 18
 vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = "CaskaydiaCove Nerd Font"
 vim.g.jumpoptions = "stack"
 vim.g.snacks_animate = false
+vim.g.root_spec =
+  { "lsp", { ".git", "lua", "Cargo.toml", "pom.xml", "build.gradle", "go.mod", "package.json", "node_modules" }, "cwd" }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,33 +1,74 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        group_empty_dirs = true,
-        scan_mode = "deep",
-      },
-      window = {
-        mappings = {
-          ["i"] = "noop",
-          ["o"] = {
-            "open",
-            nowait = true,
-          },
-          ["oc"] = "noop",
-          ["od"] = "noop",
-          ["og"] = "noop",
-          ["om"] = "noop",
-          ["on"] = "noop",
-          ["os"] = "noop",
-          ["ot"] = "noop",
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      group_empty_dirs = true,
+      scan_mode = "deep",
+    },
+    window = {
+      mappings = {
+        ["i"] = "noop",
+        ["o"] = {
+          "open",
+          nowait = true,
         },
+        ["oc"] = "noop",
+        ["od"] = "noop",
+        ["og"] = "noop",
+        ["om"] = "noop",
+        ["on"] = "noop",
+        ["os"] = "noop",
+        ["ot"] = "noop",
       },
     },
   },
-  {
-    "iamcco/markdown-preview.nvim",
-    config = function()
-      vim.g.mkdp_echo_preview_url = true
-    end,
+  keys = {
+    {
+      "-",
+      mode = { "n" },
+      function()
+        local reveal_file = vim.fn.expand("%:p")
+        if reveal_file == "" then
+          reveal_file = vim.fn.getcwd()
+        else
+          local f = io.open(reveal_file, "r")
+          if f then
+            f.close(f)
+          else
+            reveal_file = vim.fn.getcwd()
+          end
+        end
+
+        local roots = LazyVim.root.detect({ all = true })
+        roots = vim
+          .iter(roots)
+          :map(function(r)
+            return r.paths or {}
+          end)
+          :flatten()
+          :totable()
+        -- Sort roots so that those prefixed by `reveal_file` come first.
+        -- Among those, shorter paths are prioritized.
+        table.sort(roots, function(a, b)
+          local a_match = vim.startswith(reveal_file, a)
+          local b_match = vim.startswith(reveal_file, b)
+
+          if a_match ~= b_match then
+            return a_match
+          end
+
+          return #a < #b
+        end)
+        local dir_root = roots[1] or LazyVim.root.cwd()
+
+        require("neo-tree.command").execute({
+          action = "focus",
+          dir = dir_root,
+          reveal_file = reveal_file, -- path to file or folder to reveal
+          reveal_force_cwd = true, -- change cwd without asking if needed
+        })
+      end,
+      desc = "Open neo-tree at current file or working directory",
+    },
   },
 }
