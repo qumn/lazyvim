e3adfaf245b3beba07f0bc3874a1302aa63c9130
qumn <wang247657@163.com>
2025-11-06 11:02:38 +0800

    updaet

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -2,26 +2,30 @@ return {
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
-    opts = function(_, opts)
-      local Keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      -- stylua: ignore start
-      vim.list_extend(Keys, {
-        { "gy", false },
-        { "gt", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto [T]ype Definition" },
-        { "gI", false },
-        { "gi", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
-      })
-      -- stylua: ignore end
-
-      return vim.tbl_deep_extend("force", opts, {
-        setup = {
-          rust_analyzer = function()
-            return true
-          end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "gy", false },
+            {
+              "gt",
+              function()
+                require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
+              end,
+              desc = "Goto [T]ype Definition",
+            },
+            { "gI", false },
+            {
+              "gi",
+              function()
+                require("telescope.builtin").lsp_implementations({ reuse_win = true })
+              end,
+              desc = "Goto Implementation",
+            },
+          },
         },
-      })
-    end,
+      },
+    },
   },
   {
     "mfussenegger/nvim-jdtls",
