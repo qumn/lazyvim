42e533a0c40b03e88c62fa5f8b0f0e7d82ef192c
qumn <wang247657@163.com>
2025-12-05 14:20:35 +0800

    uppdate

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -19,7 +19,34 @@ return {
         ["on"] = "noop",
         ["os"] = "noop",
         ["ot"] = "noop",
+        ["l"] = "noop",
+        ["lf"] = "system_explorer",
       },
+    },
+    commands = {
+      system_explorer = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        local uname = vim.loop.os_uname()
+
+        if uname.sysname == "Darwin" then
+          -- macOs: open file in default application in the background.
+          vim.fn.jobstart({ "open", "-R", path }, { detach = true })
+        elseif uname.sysname == "Linux" then
+          -- Linux: open file in default application
+          vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+        elseif uname.sysname == "Windows" then
+          -- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+          local p
+          local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+          if lastSlashIndex then
+            p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
+          else
+            p = path -- If no slash found, return original path
+          end
+          vim.cmd("silent !start explorer " .. p)
+        end
+      end,
     },
   },
   keys = {
