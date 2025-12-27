5befc52e96f87851ea97a8f7a35fb037a87fe6d9
qumn <wang247657@163.com>
2025-12-27 21:05:12 +0800

    feat: tweak window keymap

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,5 +1,6 @@
 return {
   "mrjones2014/smart-splits.nvim",
+  dependencies = { "pogyomo/submode.nvim" },
   event = "BufRead",
   enable = not vim.g.neovide,
   init = function()
@@ -9,5 +10,44 @@ return {
     map("n", "<C-i>", require("smart-splits").move_cursor_up)
     map("n", "<C-o>", require("smart-splits").move_cursor_right)
     map("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+
+    map("n", "<C-S-Y>", require("smart-splits").resize_left)
+    map("n", "<C-S-N>", require("smart-splits").resize_down)
+    map("n", "<C-S-I>", require("smart-splits").resize_up)
+    map("n", "<C-S-O>", require("smart-splits").resize_right)
+
+    map("n", "<leader><leader>y", require("smart-splits").swap_buf_left)
+    map("n", "<leader><leader>n", require("smart-splits").swap_buf_down)
+    map("n", "<leader><leader>i", require("smart-splits").swap_buf_up)
+    map("n", "<leader><leader>o", require("smart-splits").swap_buf_right)
+
+    -- Resize
+    local submode = require("submode")
+    submode.create("WinResize", {
+      mode = "n",
+      enter = "<C-w>r",
+      leave = { "<Esc>", "q", "<C-c>" },
+      hook = {
+        on_enter = function()
+          vim.notify("Use { y, n, i, o } or { <Left>, <Down>, <Up>, <Right> } to resize the window")
+        end,
+        on_leave = function()
+          vim.notify("")
+        end,
+      },
+      default = function(register)
+        register("y", require("smart-splits").resize_left, { desc = "Resize left" })
+        register("n", require("smart-splits").resize_down, { desc = "Resize down" })
+        register("i", require("smart-splits").resize_up, { desc = "Resize up" })
+        register("o", require("smart-splits").resize_right, { desc = "Resize right" })
+        register("s", "<CMD>split<CR>", { desc = "Create horizontal split" })
+        register("v", "<CMD>vsplit<CR>", { desc = "Create vertical split" })
+        register("f", "<CMD>Telescope find_files<CR>", { desc = "Find files" })
+        register("<Left>", require("smart-splits").resize_left, { desc = "Resize left" })
+        register("<Down>", require("smart-splits").resize_down, { desc = "Resize down" })
+        register("<Up>", require("smart-splits").resize_up, { desc = "Resize up" })
+        register("<Right>", require("smart-splits").resize_right, { desc = "Resize right" })
+      end,
+    })
   end,
 }
