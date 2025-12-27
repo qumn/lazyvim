072213f3701047e4970bfe99e4ca42b4f582b9ab
qumn <wang247657@163.com>
2025-12-27 11:01:15 +0800

    telescope: add live_grep suffix prompt

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,6 +22,8 @@ return {
     },
     opts = function(_, opts)
       local actions = require("telescope.actions")
+      local action_state = require("telescope.actions.state")
+      local builtin = require("telescope.builtin")
       local trouble_telescope = require("trouble.sources.telescope")
       local function open_with_trouble_focus(prompt_bufnr)
         trouble_telescope.open(prompt_bufnr, { focus = true, source = "telescope" })
@@ -33,6 +35,53 @@ return {
       end
 
       local prev = "<C-i>"
+      local function live_grep_suffixes(prompt_bufnr)
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        if not picker or type(picker.prompt_title) ~= "string" then
+          return
+        end
+        local title = picker.prompt_title
+        if not title:find("Live Grep", 1, true) then
+          return
+        end
+        local default = title:match("^Live Grep %((.+)%)$") or ""
+        local input_opts = { prompt = "File suffixes (comma): " }
+        if default ~= "" then
+          input_opts.default = default
+        end
+        vim.ui.input(input_opts, function(input)
+          if input == nil then
+            return
+          end
+          local args = {}
+          local label = input:gsub("%s+", "")
+          if label ~= "" then
+            for ext in string.gmatch(label, "[^,%s]+") do
+              local suffix = ext
+              if suffix:sub(1, 1) == "." then
+                suffix = "*" .. suffix
+              else
+                suffix = "*." .. suffix
+              end
+              table.insert(args, "-g")
+              table.insert(args, suffix)
+            end
+          end
+          actions.close(prompt_bufnr)
+          vim.schedule(function()
+            if label == "" or #args == 0 then
+              builtin.live_grep()
+              return
+            end
+            builtin.live_grep({
+              additional_args = function()
+                return args
+              end,
+              prompt_title = "Live Grep (" .. label .. ")",
+            })
+          end)
+        end)
+      end
 
       return vim.tbl_extend("force", opts, {
         defaults = {
@@ -73,6 +122,7 @@ return {
               ["O"] = actions.move_to_bottom,
               ["s"] = actions.select_horizontal,
               ["v"] = actions.select_vertical,
+              ["f"] = live_grep_suffixes,
               ["<C-q>"] = open_with_trouble_focus,
             },
           },
