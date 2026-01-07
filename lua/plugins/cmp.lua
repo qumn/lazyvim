aa3df0781551568c02132cf9c5d12c5d45615e23
qumn <wang247657@163.com>
2026-01-07 14:06:02 +0800

    feat: 调整 Tab ai 功能优先级

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -31,14 +31,23 @@ return {
 
         ["<Tab>"] = {
           function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
+            -- if menu is visible and item is manually selected, priority accept it
+            local list = require("blink.cmp.completion.list")
+            local explicit = list.is_explicitly_selected
+            if type(explicit) == "function" then
+              explicit = explicit()
             end
+            local is_manual_selected = cmp.is_menu_visible() and (explicit == true)
+            if is_manual_selected then
+              return cmp.accept()
+            end
+
+            return LazyVim.cmp.map({
+              "snippet_forward",
+              "ai_nes",
+              "ai_accept",
+            })() or cmp.select_and_accept()
           end,
-          -- setup for Copilot native integration
-          LazyVim.cmp.map({ "snippet_forward", "ai_nes", "ai_accept" }),
           "fallback",
         },
         ["<CR>"] = { "accept", "fallback" },
@@ -68,6 +77,24 @@ return {
         },
         menu = {
           border = "rounded",
+          direction_priority = function()
+            local ctx = require("blink.cmp").get_context()
+            local item = require("blink.cmp").get_selected_item()
+            if ctx == nil or item == nil then
+              return { "s", "n" }
+            end
+
+            local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
+            local is_multi_line = item_text:find("\n") ~= nil
+
+            -- after showing the menu upwards, we want to maintain that direction
+            -- until we re-open the menu, so store the context id in a global variable
+            if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+              vim.g.blink_cmp_upwards_ctx_id = ctx.id
+              return { "n", "s" }
+            end
+            return { "s", "n" }
+          end,
           draw = {
             -- padding = 1,
             -- gap = 4,
